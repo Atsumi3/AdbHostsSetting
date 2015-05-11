44c2fba8a19e0a912601c0791c3f991c150a6505
atsumi <atsumi3041@yahoo.co.jp>
2015-05-11 21:58:00 +0900

    first commit

---
+++ AdbHostsSetting/MainForm.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace AdbHostsSetting
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.adbList = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bRefreshDevices = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LADBPath = new System.Windows.Forms.Label();
+            this.LCurrentDevice = new System.Windows.Forms.Label();
+            this.bRunOpenHosts = new System.Windows.Forms.Button();
+            this.LSystemBlock = new System.Windows.Forms.Label();
+            this.LReMount = new System.Windows.Forms.Label();
+            this.LChangePermission = new System.Windows.Forms.Label();
+            this.LPullFiles = new System.Windows.Forms.Label();
+            this.LPushFiles = new System.Windows.Forms.Label();
+            this.bSaveHostsFromPC = new System.Windows.Forms.Button();
+            this.bOpenHostsFromAndroid = new System.Windows.Forms.Button();
+            this.debugText = new System.Windows.Forms.TextBox();
+            this.bSelectADBPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // adbList
+            // 
+            this.adbList.Location = new System.Drawing.Point(12, 54);
+            this.adbList.Name = "adbList";
+            this.adbList.Size = new System.Drawing.Size(200, 354);
+            this.adbList.TabIndex = 0;
+            this.adbList.UseCompatibleStateImageBehavior = false;
+            this.adbList.SelectedIndexChanged += new System.EventHandler(this.adbList_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ADB List";
+            // 
+            // bRefreshDevices
+            // 
+            this.bRefreshDevices.Location = new System.Drawing.Point(12, 28);
+            this.bRefreshDevices.Name = "bRefreshDevices";
+            this.bRefreshDevices.Size = new System.Drawing.Size(102, 20);
+            this.bRefreshDevices.TabIndex = 2;
+            this.bRefreshDevices.Text = "Refresh Devices";
+            this.bRefreshDevices.UseVisualStyleBackColor = true;
+            this.bRefreshDevices.Click += new System.EventHandler(this.bRefreshDevices_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ADB PATH : ";
+            // 
+            // LADBPath
+            // 
+            this.LADBPath.AutoSize = true;
+            this.LADBPath.Location = new System.Drawing.Point(168, 8);
+            this.LADBPath.Name = "LADBPath";
+            this.LADBPath.Size = new System.Drawing.Size(0, 12);
+            this.LADBPath.TabIndex = 4;
+            // 
+            // LCurrentDevice
+            // 
+            this.LCurrentDevice.AutoSize = true;
+            this.LCurrentDevice.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.LCurrentDevice.Location = new System.Drawing.Point(217, 32);
+            this.LCurrentDevice.Name = "LCurrentDevice";
+            this.LCurrentDevice.Size = new System.Drawing.Size(78, 12);
+            this.LCurrentDevice.TabIndex = 6;
+            this.LCurrentDevice.Text = "CurrentDevice";
+            // 
+            // bRunOpenHosts
+            // 
+            this.bRunOpenHosts.Location = new System.Drawing.Point(219, 76);
+            this.bRunOpenHosts.Name = "bRunOpenHosts";
+            this.bRunOpenHosts.Size = new System.Drawing.Size(109, 23);
+            this.bRunOpenHosts.TabIndex = 7;
+            this.bRunOpenHosts.Text = "Run";
+            this.bRunOpenHosts.UseVisualStyleBackColor = true;
+            this.bRunOpenHosts.Click += new System.EventHandler(this.bRunOpenHosts_Click);
+            // 
+            // LSystemBlock
+            // 
+            this.LSystemBlock.AutoSize = true;
+            this.LSystemBlock.Location = new System.Drawing.Point(219, 115);
+            this.LSystemBlock.Name = "LSystemBlock";
+            this.LSystemBlock.Size = new System.Drawing.Size(90, 12);
+            this.LSystemBlock.TabIndex = 8;
+            this.LSystemBlock.Text = "SYSTEM_BLOCK";
+            // 
+            // LReMount
+            // 
+            this.LReMount.AutoSize = true;
+            this.LReMount.Location = new System.Drawing.Point(218, 143);
+            this.LReMount.Name = "LReMount";
+            this.LReMount.Size = new System.Drawing.Size(60, 12);
+            this.LReMount.TabIndex = 10;
+            this.LReMount.Text = "REMOUNT";
+            // 
+            // LChangePermission
+            // 
+            this.LChangePermission.AutoSize = true;
+            this.LChangePermission.Location = new System.Drawing.Point(218, 172);
+            this.LChangePermission.Name = "LChangePermission";
+            this.LChangePermission.Size = new System.Drawing.Size(72, 12);
+            this.LChangePermission.TabIndex = 12;
+            this.LChangePermission.Text = "PERMISSION";
+            // 
+            // LPullFiles
+            // 
+            this.LPullFiles.AutoSize = true;
+            this.LPullFiles.Location = new System.Drawing.Point(218, 200);
+            this.LPullFiles.Name = "LPullFiles";
+            this.LPullFiles.Size = new System.Drawing.Size(32, 12);
+            this.LPullFiles.TabIndex = 14;
+            this.LPullFiles.Text = "PULL";
+            // 
+            // LPushFiles
+            // 
+            this.LPushFiles.AutoSize = true;
+            this.LPushFiles.Location = new System.Drawing.Point(334, 235);
+            this.LPushFiles.Name = "LPushFiles";
+            this.LPushFiles.Size = new System.Drawing.Size(35, 12);
+            this.LPushFiles.TabIndex = 16;
+            this.LPushFiles.Text = "PUSH";
+            // 
+            // bSaveHostsFromPC
+            // 
+            this.bSaveHostsFromPC.Location = new System.Drawing.Point(219, 230);
+            this.bSaveHostsFromPC.Name = "bSaveHostsFromPC";
+            this.bSaveHostsFromPC.Size = new System.Drawing.Size(109, 23);
+            this.bSaveHostsFromPC.TabIndex = 15;
+            this.bSaveHostsFromPC.Text = "Save File";
+            this.bSaveHostsFromPC.UseVisualStyleBackColor = true;
+            this.bSaveHostsFromPC.Click += new System.EventHandler(this.bSaveHostsFromPC_Click);
+            // 
+            // bOpenHostsFromAndroid
+            // 
+            this.bOpenHostsFromAndroid.ForeColor = System.Drawing.Color.Black;
+            this.bOpenHostsFromAndroid.Location = new System.Drawing.Point(219, 259);
+            this.bOpenHostsFromAndroid.Name = "bOpenHostsFromAndroid";
+            this.bOpenHostsFromAndroid.Size = new System.Drawing.Size(109, 23);
+            this.bOpenHostsFromAndroid.TabIndex = 17;
+            this.bOpenHostsFromAndroid.Text = "Open File";
+            this.bOpenHostsFromAndroid.UseVisualStyleBackColor = true;
+            this.bOpenHostsFromAndroid.Click += new System.EventHandler(this.bOpenHostsFromAndroid_Click);
+            // 
+            // debugText
+            // 
+            this.debugText.Location = new System.Drawing.Point(219, 289);
+            this.debugText.Multiline = true;
+            this.debugText.Name = "debugText";
+            this.debugText.Size = new System.Drawing.Size(553, 108);
+            this.debugText.TabIndex = 18;
+            // 
+            // bSelectADBPath
+            // 
+            this.bSelectADBPath.Location = new System.Drawing.Point(12, 3);
+            this.bSelectADBPath.Name = "bSelectADBPath";
+            this.bSelectADBPath.Size = new System.Drawing.Size(75, 23);
+            this.bSelectADBPath.TabIndex = 19;
+            this.bSelectADBPath.Text = "adb Path";
+            this.bSelectADBPath.UseVisualStyleBackColor = true;
+            this.bSelectADBPath.Click += new System.EventHandler(this.bSelectADBPath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 420);
+            this.Controls.Add(this.bRunOpenHosts);
+            this.Controls.Add(this.LSystemBlock);
+            this.Controls.Add(this.debugText);
+            this.Controls.Add(this.bSelectADBPath);
+            this.Controls.Add(this.LReMount);
+            this.Controls.Add(this.LCurrentDevice);
+            this.Controls.Add(this.bOpenHostsFromAndroid);
+            this.Controls.Add(this.LADBPath);
+            this.Controls.Add(this.LChangePermission);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LPushFiles);
+            this.Controls.Add(this.bRefreshDevices);
+            this.Controls.Add(this.LPullFiles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bSaveHostsFromPC);
+            this.Controls.Add(this.adbList);
+            this.Name = "Form1";
+            this.Text = "AdbHostsSetting";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView adbList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bRefreshDevices;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LADBPath;
+        private System.Windows.Forms.Label LCurrentDevice;
+        private System.Windows.Forms.Button bRunOpenHosts;
+        private System.Windows.Forms.Label LSystemBlock;
+        private System.Windows.Forms.Label LReMount;
+        private System.Windows.Forms.Label LChangePermission;
+        private System.Windows.Forms.Label LPullFiles;
+        private System.Windows.Forms.Label LPushFiles;
+        private System.Windows.Forms.Button bSaveHostsFromPC;
+        private System.Windows.Forms.Button bOpenHostsFromAndroid;
+        private System.Windows.Forms.TextBox debugText;
+        private System.Windows.Forms.Button bSelectADBPath;
+    }
+}
+
---
+++ AdbHostsSetting/MainForm.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AdbHostsSetting
+{
+    public partial class MainForm : Form
+    {
+        public static string TAG_CURRENT_DEVICE = "DEVICE : ";
+        public static string ER_DEVICE_NOT_FOUND = "device not found.";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeListView();
+            LCurrentDevice.Text = TAG_CURRENT_DEVICE;
+            runTimer();
+        }
+
+        /******** Initialie Method ********/
+
+        public void InitializeListView()
+        {
+            adbList.View = View.Details;
+            adbList.FullRowSelect = true;
+
+            ColumnHeader hDevices = new ColumnHeader();
+            hDevices.Text = "devices";
+            hDevices.TextAlign = HorizontalAlignment.Center;
+            ColumnHeader hAttached = new ColumnHeader();
+            hAttached.Text = "attached";
+            hAttached.TextAlign = HorizontalAlignment.Center;
+            ColumnHeader[] columnHeader = { hDevices, hAttached };
+            adbList.Columns.AddRange(columnHeader);
+            adbList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        /******** My Method ********/
+
+        private void runTimer()
+        {
+            //new System.Threading.Timer(new TimerCallback(TimerRefreshDeviceList), null, 0, 1000);
+        }
+
+        /// <summary>
+        /// Timer Method - auto command 'adb devices'
+        /// </summary>
+        private void TimerRefreshDeviceList(object o)
+        {
+            List<ListViewItem> l = new List<ListViewItem>();
+            foreach (string name in ADB_SETTING.getSingleton(this).getDevices())
+            {
+                string[] items = name.Split('\t');
+                l.Add(new ListViewItem(new string[] { items[0], items[1] }));
+            }
+            Invoke(new TimerDelegateAddList(addList), l);
+        }
+
+
+        /// <summary>
+        /// Timer -Delegate Add List
+        /// </summary>
+        /// <param name="items"></param>
+        delegate void TimerDelegateAddList(List<ListViewItem> items);
+        private void addList(List<ListViewItem> items)
+        {
+            ADB_SETTING.getSingleton(this).init();
+            adbList.Items.Clear();
+            foreach(ListViewItem item in items){
+                adbList.Items.Add(item);
+            }
+            adbList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        /******** Event Listener ********/
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            LADBPath.Text = ADB_SETTING.getSingleton(this).PATH;
+        }
+
+        private void adbList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection _ = this.adbList.SelectedItems;
+            if (1 > _.Count) return;
+            ListViewItem item = _[0];
+            if (item == null) return;
+            LCurrentDevice.Text = TAG_CURRENT_DEVICE + item.SubItems[0].Text;
+            ADB_SETTING.getSingleton(this).DEVICE_NAME = item.SubItems[0].Text;
+        }
+
+        private void bRunOpenHosts_Click(object sender, EventArgs e)
+        {
+            ADB_SETTING adbSetting = ADB_SETTING.getSingleton(this);
+
+            LSystemBlock.Text = adbSetting.getSystemDirBlock();
+
+            string ret = adbSetting.setRemount();
+            if (ret.Contains("Operation"))
+            {
+                MessageBox.Show(ret, "REMOUNT FAILED");
+                LReMount.Text = "REMOUNT FAILED";
+                return;
+            }
+            else
+            {
+                LReMount.Text = "REMOUNT SUCCESS";
+            }
+
+            ret = adbSetting.changePermission();
+            LChangePermission.Text = ret.Equals("") ? "CHMOD OK" : ret;
+
+            ret = adbSetting.getHostsFile();
+            LPullFiles.Text = ret.Equals("") ? "PULL OK":ret;
+        }
+
+        private void bSaveHostsFromPC_Click(object sender, EventArgs e)
+        {
+            ADB_SETTING adbSetting = ADB_SETTING.getSingleton(this);
+            string ret = adbSetting.pushHostsfile();
+            LPushFiles.Text = ret.Equals("")?"PUSH OK":ret;
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ADB_SETTING.getSingleton(this).deleteLocalHostsFile();
+        }
+
+        private void bOpenHostsFromAndroid_Click(object sender, EventArgs e)
+        {
+            debugText.Text = ADB_SETTING.getSingleton(this).catHostsFile();
+        }
+
+        private void bSelectADBPath_Click(object sender, EventArgs e)
+        {
+            LADBPath.Text = ADB_SETTING.getSingleton(this).openFile();
+        }
+
+        private void bRefreshDevices_Click(object sender, EventArgs e)
+        {
+            List<ListViewItem> l = new List<ListViewItem>();
+            foreach (string name in ADB_SETTING.getSingleton(this).getDevices())
+            {
+                string[] items = name.Split('\t');
+                l.Add(new ListViewItem(new string[] { items[0], items[1] }));
+            }
+
+            ADB_SETTING.getSingleton(this).init();
+            adbList.Items.Clear();
+            foreach (ListViewItem item in l)
+            {
+                adbList.Items.Add(item);
+            }
+            adbList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+    }
+
+
+    class ADB_SETTING
+    {
+        private string DEFAULT_ADB_PATH = "F:\\android-sdk\\sdk\\platform-tools\\adb.exe";
+        private string SYSTEM_DIR = "/system";
+        private string EXECUTABLE_DIR = Application.StartupPath;
+        static ADB_SETTING instance = new ADB_SETTING();
+        public string PATH { get;private set; }
+        public string DEVICE_NAME { get; set; }
+        public string SYSTEM_PATH { get; private set; }
+        private ADB_SETTING()
+        {
+            this.PATH = DEFAULT_ADB_PATH;
+            init();
+        }
+        public void init()
+        {
+            this.DEVICE_NAME = "";
+            this.SYSTEM_PATH = "";
+        }
+        public static ADB_SETTING getSingleton(Form context)
+        {
+            return instance;
+        }
+        public bool isPathSeted()
+        {
+            return !PATH.Equals("");
+        }
+        public List<string> getDevices()
+        {
+            List<string> ret = this.runCommand("devices").Split(new String[]{Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            ret.RemoveAt(0);
+            return ret;
+        }
+        public string getSystemDirBlock()
+        {
+            List<string> ret = runDeviceCommand("shell mount", true);
+            string block = "";
+            foreach (string val in ret)
+            {
+                if (val.IndexOf(this.SYSTEM_DIR) > 0)
+                {
+                    block = val;
+                    break;
+                }
+            }
+            if (!block.Equals(""))
+            {
+                this.SYSTEM_PATH = block.Split(' ')[0];
+            }
+            else
+            {
+                this.SYSTEM_PATH = "";
+            }
+            return this.SYSTEM_PATH;
+        }
+
+        public string setRemount()
+        {
+            string ret = 
+                this.runDeviceCommand("shell mount -o rw,remount " + this.SYSTEM_PATH + " " + this.SYSTEM_DIR)
+                    .Replace(Environment.NewLine, "");
+            return ret;
+        }
+
+        public string changePermission()
+        {
+            string ret =
+                this.runDeviceCommand("shell chmod 666 " + this.SYSTEM_DIR + "/etc/hosts");
+            return ret;
+        }
+
+        public string getHostsFile()
+        {
+            string ret =
+                this.runDeviceCommand("pull " + this.SYSTEM_DIR + "/etc/hosts " + this.EXECUTABLE_DIR);
+            if (File.Exists(this.EXECUTABLE_DIR +"/hosts"))
+            {
+                Process.Start(this.EXECUTABLE_DIR + "/hosts");
+                return "";
+            }
+            else
+            {
+                return ret.Equals("")?"PULL FAILED":ret;
+            }
+        }
+
+        public string pushHostsfile()
+        {
+            //if (!this.F_CHANGE_PERMISSION) return "not change permission";
+            
+            if (File.Exists(this.EXECUTABLE_DIR + "/hosts"))
+            {
+                return this.runDeviceCommand("push " + this.EXECUTABLE_DIR + "/hosts " + this.SYSTEM_DIR + "/etc/hosts");
+            }
+            else
+            {
+                return "file not exists...\n" + this.EXECUTABLE_DIR + "/hosts";
+            }
+        
+        }
+        public string catHostsFile()
+        {
+            return this.runDeviceCommand("shell cat " + this.SYSTEM_DIR + "/etc/hosts");
+        }
+
+        private string runCommand(string command)
+        {
+            if (this.PATH == null || this.PATH.Equals(""))
+            {
+                throw new FileNotFoundException("not set path <adb.exe>");
+            }
+            Process p = new Process();
+            p.StartInfo.FileName = this.PATH;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.Arguments = command;
+
+            p.Start();
+            string results = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+            return results;
+        }
+        private List<string> runCommand(string command, bool emptyRemove)
+        {
+            if (this.PATH == null || this.PATH.Equals(""))
+            {
+                throw new FileNotFoundException("not set path <adb.exe>");
+            }
+            Process p = new Process();
+            p.StartInfo.FileName = this.PATH;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.Arguments = command;
+
+            p.Start();
+            string results = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+
+            return results.Split(new string[] { Environment.NewLine },
+                emptyRemove?StringSplitOptions.RemoveEmptyEntries:StringSplitOptions.None).ToList();
+        }
+
+        private string runDeviceCommand(string command)
+        {
+            if (this.DEVICE_NAME == null || this.DEVICE_NAME.Equals("")) return MainForm.ER_DEVICE_NOT_FOUND;
+            return runCommand("-s " + this.DEVICE_NAME + " " + command);
+        }
+
+        private List<string> runDeviceCommand(string command, bool emptyRemove)
+        {
+            if (this.DEVICE_NAME == null || this.DEVICE_NAME.Equals("")) return new List<string>();
+            return runCommand("-s " + this.DEVICE_NAME + " " + command, emptyRemove);
+        }
+
+        public string openFile()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = @"C:\";
+            dialog.Filter = "ExeFile(*.exe)|*.exe";
+            dialog.Title = "Select adb.exe";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.PATH = dialog.FileName;
+                return dialog.FileName;
+            }
+            else
+            {
+                return this.DEFAULT_ADB_PATH;
+            }
+        }
+        public void deleteLocalHostsFile()
+        {
+            if (File.Exists(this.EXECUTABLE_DIR + "/hosts"))
+            {
+                File.Delete(this.EXECUTABLE_DIR + "/hosts");
+            }
+        }
+    }
+}
